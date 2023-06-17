6fca501b259b205554d6a950c8f88883f20f4c51
JovanaKrstevska <100038564+JovanaKrstevska@users.noreply.github.com>
2023-06-18 01:09:06 +0200

    Complete RegisterActivity

--- app/src/main/java/mk/ukim/finki/examiner/RegisterActivity.kt
+++ app/src/main/java/mk/ukim/finki/examiner/RegisterActivity.kt
@@ -2,10 +2,12 @@ package mk.ukim.finki.examiner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
+
 }
