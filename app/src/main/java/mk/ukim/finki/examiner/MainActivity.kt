94ff83d605674e9c1adf54418d1ca521d993c477
JovanaKrstevska <100038564+JovanaKrstevska@users.noreply.github.com>
2023-06-18 02:03:55 +0200

    Complete LoginActivity

--- app/src/main/java/mk/ukim/finki/examiner/MainActivity.kt
+++ app/src/main/java/mk/ukim/finki/examiner/MainActivity.kt
@@ -11,15 +11,21 @@ import mk.ukim.finki.examiner.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnGoToRegisterActivity: Button
+    private lateinit var btnGoToLogInActivity: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnGoToRegisterActivity = findViewById(R.id.btnGoToRegisterActivity)
+        btnGoToLogInActivity = findViewById(R.id.btnGoToLogInActivity)
 
         btnGoToRegisterActivity.setOnClickListener{
             val Intent:Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(Intent)
+        }
+        btnGoToLogInActivity.setOnClickListener{
+            val Intent:Intent = Intent(this, LoginActivity::class.java)
             startActivity(Intent)
         }
     }
