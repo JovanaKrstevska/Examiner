b1f7149fc170997e9382f2ed2b18ca3f970a4726
JovanaKrstevska <100038564+JovanaKrstevska@users.noreply.github.com>
2023-06-21 12:38:59 +0200

    Create Firebase auth and connect

--- app/src/main/java/mk/ukim/finki/examiner/RegisterActivity.kt
+++ app/src/main/java/mk/ukim/finki/examiner/RegisterActivity.kt
@@ -1,13 +1,31 @@
 package mk.ukim.finki.examiner
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var nameAndsurname: EditText
+    private lateinit var email: EditText
+    private lateinit var passord: EditText
+    private lateinit var confirmPassword: EditText
+    private lateinit var photo: ImageView
+    private lateinit var btnGoToDashboardActivity: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        nameAndsurname = findViewById(R.id.writeFirstAndLastName)
+        email = findViewById(R.id.writeEmail)
+        passord = findViewById(R.id.writePassword)
+        confirmPassword = findViewById(R.id.confirmPassword)
+        photo = findViewById(R.id.uploadPhoto)
+        btnGoToDashboardActivity = findViewById(R.id.btnGoToDashboardActivity)
     }
 
 }
